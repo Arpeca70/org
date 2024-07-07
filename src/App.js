7bc0815fe88d31e3b33e4f5e6b24d5fdf37d43a1
Arturo Perea <arpeca70@yahoo.com>
2024-07-07 14:29:59 -0700

    Cambio de Fuetes index.css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './componet/header/header';
+
+console.log(Header)
 
 function App() {
   return (
@@ -15,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender React
         </a>
       </header>
     </div>
