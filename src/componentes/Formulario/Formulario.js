d7945670f4c4e020f0c312a5b2d0d5a199a6ec92
Arturo Perea <arpeca70@yahoo.com>
2024-07-07 21:30:18 -0700

    Boton de mostrar y ocultar Formulario

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -3,15 +3,22 @@ import "./Formulario.css"
 //porque su nombre es index.js y ese es el nombre por default.
 import CampoTexto from "../CampoTexto"
 import ListaOpciones from "../ListaOpciones"
+import Boton from "../Boton"
 
 const Formulario = () => {
+
+    const manejarEnvio = (event)=> {
+        event.preventDefault()
+    }
+
     return <section className="formulario">
-            <form >
+            <form onSubmit={manejarEnvio}>
                 <h2>Rellena el formulario para crear el colaborador.</h2>
-                <CampoTexto titulo="Nombre" placeholder="Ingresar Nombre" />
-                <CampoTexto titulo="Puesto" placeholder="Ingresar Puesto" />
-                <CampoTexto titulo="Foto" placeholder="Ingresar Foto" />
+                <CampoTexto titulo="Nombre" placeholder="Ingresar Nombre" required />
+                <CampoTexto titulo="Puesto" placeholder="Ingresar Puesto" required />
+                <CampoTexto titulo="Foto" placeholder="Ingresar Foto" required />
                 <ListaOpciones />
+                <Boton>Crear</Boton>
             </form>
         </section>
 }
