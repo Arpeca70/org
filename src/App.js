bb0e8b4b314fcabfb9755a69a27055bc8d1149e3
Arturo Perea <arpeca70@yahoo.com>
2024-07-11 18:43:55 -0700

    crear eliminar colaborador

--- src/App.js
+++ src/App.js
@@ -89,46 +89,11 @@ function App() {
   //spread operator
     actualizarColaboradores([...colaboradores,colaborador])
   }
+  
+  const eliminarColaborador = ()=>{
+    console.log("Eliminar Colaborador")
+  }
 
-  // Lista de equipos (arreglo de objetos)
-  /*const equipos = [
-    {
-      titulo: "Programacion",
-      colorPrimario: "#57C278",
-      colorSecundario: "#D9F7E9"
-    },
-    {
-      titulo: "Fornt End",
-      colorPrimario: "#82CFFA",
-      colorSecundario: "#E8F8FF"
-    },
-    {
-      titulo: "Data Science",
-      colorPrimario: "#A6D157",
-      colorSecundario: "#F0F8E2"
-    },
-    {
-      titulo: "Devops",
-      colorPrimario: "#E06B69",
-      colorSecundario: "#FDE7E8"
-    },
-    {
-      titulo: "UX y Diseño",
-      colorPrimario: "#DB6EBF",
-      colorSecundario: "#FAE9F5"
-    },
-    {
-      titulo: "Movil",
-      colorPrimario: "#FFBA05",
-      colorSecundario: "#FFF5D9"
-    },
-    {
-      titulo: "Innovacion y Gestion",
-      colorPrimario: "#FF8A29",
-      colorSecundario: "#FFEEDF"
-    }
-  ]
-*/
   return (
     <div>
       <Header/>
@@ -156,6 +121,7 @@ function App() {
               datos={equipo} 
               key={equipo.titulo} 
               colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+              eliminarColaborador={eliminarColaborador}
             />
           )
         }
